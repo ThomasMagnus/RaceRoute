8be8704ce2d2aca040d51ac958af59593af0251b
Eldar Magomedov <emmagomedov_95@inbox.ru>
2024-02-25 12:19:07 +0300

    change files

--- src/services/services.jsx
+++
@@ -1,7 +0,0 @@
-export const getCoords = async (url) => {
-    const res = await fetch(url);
-
-    if (res.status !== 200) throw new Error("Ошибка соединения с сервером");
-
-    return await res.json();
-}
